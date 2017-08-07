4cec7b9504bc98cc6e6590ad4568bef62bb4bdd4
Lars Wander <lwander@google.com>
2017-08-07 10:13:45 -0400

    Fix util package after change openapi update

--- kubernetes/src/main/java/io/kubernetes/client/ApiClient.java
+++ kubernetes/src/main/java/io/kubernetes/client/ApiClient.java
@@ -90,6 +90,7 @@ public class ApiClient {
         // Setup authentications (key: authentication name, value: authentication).
         authentications = new HashMap<String, Authentication>();
         authentications.put("BearerToken", new ApiKeyAuth("header", "authorization"));
+        authentications.put("BasicAuth", new HttpBasicAuth());
         // Prevent the authentications from being modified.
         authentications = Collections.unmodifiableMap(authentications);
     }
