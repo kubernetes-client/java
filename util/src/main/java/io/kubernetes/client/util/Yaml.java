a69e5a591bf46037e1ffa54d118c595de04ad252
Arch Menzies <am3g15@soton.ac.uk>
2018-04-20 11:28:53 +0100

    Format files with com.coveo:fmt-maven-plugin

--- util/src/main/java/io/kubernetes/client/util/Yaml.java
+++ util/src/main/java/io/kubernetes/client/util/Yaml.java
@@ -13,6 +13,7 @@ limitations under the License.
 package io.kubernetes.client.util;
 
 import com.google.common.reflect.ClassPath;
+import io.kubernetes.client.custom.IntOrString;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,8 +22,6 @@ import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
-import io.kubernetes.client.custom.IntOrString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.constructor.Constructor;
@@ -30,7 +29,8 @@ import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.ScalarNode;
 
 public class Yaml {
-  private static org.yaml.snakeyaml.Yaml yaml = new org.yaml.snakeyaml.Yaml(new CustomConstructor());
+  private static org.yaml.snakeyaml.Yaml yaml =
+      new org.yaml.snakeyaml.Yaml(new CustomConstructor());
   private static Map<String, Class<?>> classes = new HashMap<>();
 
   static final Logger logger = LoggerFactory.getLogger(Yaml.class);
@@ -191,15 +191,12 @@ public class Yaml {
     return yaml.loadAs(reader, clazz);
   }
 
-
-  /**
-   * Defines constructor logic for custom types in this library.
-   */
+  /** Defines constructor logic for custom types in this library. */
   public static class CustomConstructor extends Constructor {
     @Override
     protected Object constructObject(Node node) {
-      if(node.getType() == IntOrString.class) {
-        return constructIntOrString((ScalarNode)node);
+      if (node.getType() == IntOrString.class) {
+        return constructIntOrString((ScalarNode) node);
       }
       return super.constructObject(node);
     }
