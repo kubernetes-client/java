68c5ad579e57f5f19e632dbab9dc35e58ddc3c46
David Xia <dxia@spotify.com>
2018-11-03 13:07:25 -0400

    Use two space indentation in some test classes
    * JSONTest
    * SuffixFormatterTest
    * V1PodBuilderTest
    
    Indent two spaces instead of four to be consistent with the rest
    of this project.

--- kubernetes/src/test/java/io/kubernetes/client/models/V1PodBuilderTest.java
+++ kubernetes/src/test/java/io/kubernetes/client/models/V1PodBuilderTest.java
@@ -7,29 +7,29 @@ import org.junit.Test;
 
 public class V1PodBuilderTest {
 
-    @Test
-    public void testBuilder() {
-        V1Pod pod1 = new V1PodBuilder()
-                .withNewMetadata()
-                    .withName("mypod")
-                .endMetadata()
-                .withNewSpec()
-                    .addNewContainer()
-                        .withName("cnt")
-                    .endContainer()
-                .endSpec()
-                .build();
+  @Test
+  public void testBuilder() {
+    V1Pod pod1 = new V1PodBuilder()
+        .withNewMetadata()
+        .withName("mypod")
+        .endMetadata()
+        .withNewSpec()
+        .addNewContainer()
+        .withName("cnt")
+        .endContainer()
+        .endSpec()
+        .build();
 
-        V1Pod pod2 = new V1Pod()
-                .metadata(new V1ObjectMeta().name("mypod"))
-                .spec(new V1PodSpec()
-                        .containers(Arrays.asList(
-                                new V1Container().name("cnt")
-                                )
-                        )
-                );
+    V1Pod pod2 = new V1Pod()
+        .metadata(new V1ObjectMeta().name("mypod"))
+        .spec(new V1PodSpec()
+            .containers(Arrays.asList(
+                new V1Container().name("cnt")
+                )
+            )
+        );
 
 
-        Assert.assertEquals(pod1, pod2);
-    }
+    Assert.assertEquals(pod1, pod2);
+  }
 }
