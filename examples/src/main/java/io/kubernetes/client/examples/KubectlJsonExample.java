98f7a3f78323b39b2c0986de4ac5a083bb0439d6
ZP-AlwaysWin <zhangpeng63@baidu.com>
2019-07-28 18:12:03 +0800

    add code

--- examples/src/main/java/io/kubernetes/client/examples/KubectlJsonExample.java
+++ examples/src/main/java/io/kubernetes/client/examples/KubectlJsonExample.java
@@ -1,17 +1,14 @@
 package io.kubernetes.client.examples;
 
-import java.io.IOException;
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.squareup.okhttp.Response;
-
 import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.util.Config;
-
+import java.io.IOException;
 
 /**
  * A simple example of how to use the Java API
@@ -21,31 +18,29 @@ import io.kubernetes.client.util.Config;
  *
  * <p>From inside $REPO_DIR/examples
  *
- * mock "kubectl get configamp configmap-name -o json"
+ * <p>mock "kubectl get configamp configmap-name -o json"
  */
-
 public class KubectlJsonExample {
 
-    public static void main(String[] args) throws ApiException, IOException {
+  public static void main(String[] args) throws ApiException, IOException {
 
-        ApiClient client = Config.defaultClient();
-        Configuration.setDefaultApiClient(client);
+    ApiClient client = Config.defaultClient();
+    Configuration.setDefaultApiClient(client);
 
-        CoreV1Api api = new CoreV1Api();
-        com.squareup.okhttp.Call call = api.readNamespacedConfigMapCall("configmap-name","default",null,null,null,null,
-                null);
+    CoreV1Api api = new CoreV1Api();
+    com.squareup.okhttp.Call call =
+        api.readNamespacedConfigMapCall("configmap-name", "default", null, null, null, null, null);
 
-        Response response = call.execute();
+    Response response = call.execute();
 
-        if(!response.isSuccessful()) {
-            return;
-        }
-
-        String body = response.body().string();
-
-        JsonObject returnData = new JsonParser().parse(body).getAsJsonObject();
-
-        System.out.println(returnData);
+    if (!response.isSuccessful()) {
+      return;
     }
 
+    String body = response.body().string();
+
+    JsonObject returnData = new JsonParser().parse(body).getAsJsonObject();
+
+    System.out.println(returnData);
+  }
 }
