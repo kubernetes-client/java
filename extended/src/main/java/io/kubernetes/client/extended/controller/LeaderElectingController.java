4ccc2d9ecdb860779653c9bfcb37866a99c7250c
yue9944882 <291271447@qq.com>
2019-11-12 12:53:45 +0800

    adding info logs on starting/stopping controller

--- extended/src/main/java/io/kubernetes/client/extended/controller/LeaderElectingController.java
+++ extended/src/main/java/io/kubernetes/client/extended/controller/LeaderElectingController.java
@@ -1,8 +1,12 @@
 package io.kubernetes.client.extended.controller;
 
 import io.kubernetes.client.extended.leaderelection.LeaderElector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LeaderElectingController implements Controller {
+
+  private static final Logger log = LoggerFactory.getLogger(LeaderElectingController.class);
 
   private Controller delegateController;
   private LeaderElector leaderElector;
@@ -20,6 +24,13 @@ public class LeaderElectingController implements Controller {
   @Override
   public void run() {
     this.leaderElector.run(
-        () -> this.delegateController.run(), () -> this.delegateController.shutdown());
+        () -> {
+          log.info("Lease acquired, starting controller..");
+          this.delegateController.run();
+        },
+        () -> {
+          log.info("Lease lost, shutting down controller..");
+          this.delegateController.shutdown();
+        });
   }
 }
