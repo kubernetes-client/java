bb94b0d5243f094cfc5905742586f9bd6621136c
yue9944882 <291271447@qq.com>
2020-03-08 21:49:12 +0800

    wipe out author signatures

--- util/src/main/java/io/kubernetes/client/util/Watchable.java
+++ util/src/main/java/io/kubernetes/client/util/Watchable.java
@@ -2,10 +2,6 @@ package io.kubernetes.client.util;
 
 import java.util.Iterator;
 
-/**
- * @author zuoxiu.jm
- * @version $Id: Watchable.java, v 0.1 2019年02月25日 5:38 PM zuoxiu.jm Exp $
- */
 public interface Watchable<T>
     extends Iterable<Watch.Response<T>>,
         Iterator<Watch.Response<T>>,
