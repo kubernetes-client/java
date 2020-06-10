7c2ef70458aaf21750b181f3ecf963faf820b3a6
sarveshkaushal <k.sarvesh19@gmail.com>
2020-06-09 21:09:34 -0700

    Adding a check to test if Tar utility is present in the target container

--- util/src/main/java/io/kubernetes/client/Copy.java
+++ util/src/main/java/io/kubernetes/client/Copy.java
@@ -115,48 +115,51 @@ public class Copy extends Exec {
   public void copyDirectoryFromPod(
       String namespace, String pod, String container, String srcPath, Path destination)
       throws ApiException, IOException {
-    // TODO: Test that 'tar' is present in the container?
-    final Process proc =
-        this.exec(
-            namespace,
-            pod,
-            new String[] {"sh", "-c", "tar cz - " + srcPath + " | base64"},
-            container,
-            false,
-            false);
-    try (InputStream is = new Base64InputStream(new BufferedInputStream(proc.getInputStream()));
-        ArchiveInputStream archive = new TarArchiveInputStream(new GzipCompressorInputStream(is))) {
-      for (ArchiveEntry entry = archive.getNextEntry();
-          entry != null;
-          entry = archive.getNextEntry()) {
-        if (!archive.canReadEntryData(entry)) {
-          log.error("Can't read: " + entry);
-          continue;
-        }
-        File f = new File(destination.toFile(), entry.getName());
-        if (entry.isDirectory()) {
-          if (!f.isDirectory() && !f.mkdirs()) {
-            throw new IOException("create directory failed: " + f);
+    // Test that 'tar' is present in the container?
+    if (isTarPresentInContainer(namespace, pod, container)) {
+      final Process proc =
+          this.exec(
+              namespace,
+              pod,
+              new String[] {"sh", "-c", "tar cz - " + srcPath + " | base64"},
+              container,
+              false,
+              false);
+      try (InputStream is = new Base64InputStream(new BufferedInputStream(proc.getInputStream()));
+          ArchiveInputStream archive =
+              new TarArchiveInputStream(new GzipCompressorInputStream(is))) {
+        for (ArchiveEntry entry = archive.getNextEntry();
+            entry != null;
+            entry = archive.getNextEntry()) {
+          if (!archive.canReadEntryData(entry)) {
+            log.error("Can't read: " + entry);
+            continue;
           }
-        } else {
-          File parent = f.getParentFile();
-          if (!parent.isDirectory() && !parent.mkdirs()) {
-            throw new IOException("create directory failed: " + parent);
-          }
-          try (OutputStream fs = new FileOutputStream(f)) {
-            ByteStreams.copy(archive, fs);
-            fs.flush();
+          File f = new File(destination.toFile(), entry.getName());
+          if (entry.isDirectory()) {
+            if (!f.isDirectory() && !f.mkdirs()) {
+              throw new IOException("create directory failed: " + f);
+            }
+          } else {
+            File parent = f.getParentFile();
+            if (!parent.isDirectory() && !parent.mkdirs()) {
+              throw new IOException("create directory failed: " + parent);
+            }
+            try (OutputStream fs = new FileOutputStream(f)) {
+              ByteStreams.copy(archive, fs);
+              fs.flush();
+            }
           }
         }
       }
-    }
-    try {
-      int status = proc.waitFor();
-      if (status != 0) {
-        throw new IOException("Copy command failed with status " + status);
+      try {
+        int status = proc.waitFor();
+        if (status != 0) {
+          throw new IOException("Copy command failed with status " + status);
+        }
+      } catch (InterruptedException ex) {
+        throw new IOException(ex);
       }
-    } catch (InterruptedException ex) {
-      throw new IOException(ex);
     }
   }
 
@@ -201,5 +204,18 @@ public class Copy extends Exec {
     }
 
     return;
+  }
+
+  private boolean isTarPresentInContainer(String namespace, String pod, String container)
+      throws ApiException, IOException {
+    final Process proc =
+        this.exec(
+            namespace, pod, new String[] {"sh", "-c", "tar --version"}, container, false, false);
+    // This will work for POSIX based operating systems
+    if (proc.exitValue() == 127) {
+      return false;
+    }
+    proc.destroy();
+    return true;
   }
 }
