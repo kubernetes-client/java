5225b964a1ea1fbcc11a46911066c76b60b0051f
sarveshkaushal <k.sarvesh19@gmail.com>
2020-07-23 03:31:53 -0700

    Reformatting the files

--- util/src/main/java/io/kubernetes/client/util/version/VersionUtil.java
+++ util/src/main/java/io/kubernetes/client/util/version/VersionUtil.java
@@ -1,3 +1,15 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package io.kubernetes.client.util.version;
 
 import com.google.gson.Gson;
@@ -31,13 +43,13 @@ public class VersionUtil {
   public VersionInfo getVersion() throws ApiException, IOException {
     Call call = versionApi.getCodeCall(null);
     Response response = null;
-    try{
+    try {
       response = call.execute();
-    }catch (IOException ex){
+    } catch (IOException ex) {
       throw new ApiException("Version request failed");
     }
 
-    if(!response.isSuccessful()){
+    if (!response.isSuccessful()) {
       throw new ApiException(response.code(), "Version request failed: " + response.code());
     }
     Gson gson = new Gson();
