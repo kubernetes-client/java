45a9527c420a7ea51dab9fef75d8b805d739f70b
yue9944882 <291271447@qq.com>
2020-09-09 10:44:00 +0800

    fixes crd validation precheck

--- client-java-contrib/prometheus-operator/update.sh
+++ client-java-contrib/prometheus-operator/update.sh
@@ -16,9 +16,6 @@
 # This script generates the model classes from a released version of cert-manager CRDs
 # under src/main/java/io/cert/manager/models.
 
-# build the crdgen image which includes all the toolchains needed for code generation
-docker build .. -t crdgen
-
 # a crdgen container is run in a way that:
 #   1. it has access to the docker daemon on the host so that it is able to create sibling container on the host
 #   2. it runs on the host network so that it is able to communicate with the KinD cluster it launched on the host
@@ -28,7 +25,8 @@ docker run \
   -v "$(pwd)":"$(pwd)" \
   -ti \
   --network host \
-  crdgen /generate.sh \
+  docker.pkg.github.com/kubernetes-client/java/crd-model-gen:v1.0.1 \
+  /generate.sh \
   -u https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.38.1/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml \
   -u https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.38.1/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml \
   -u https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.38.1/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml \
