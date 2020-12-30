f7279e1583b09d76b1febb55fcc845d8a2b905e6
yue9944882 <291271447@qq.com>
2020-12-30 15:08:57 +0800

    swap javadoc for filter annotations

--- spring/src/main/java/io/kubernetes/client/spring/extended/controller/annotation/DeleteWatchEventFilter.java
+++ spring/src/main/java/io/kubernetes/client/spring/extended/controller/annotation/DeleteWatchEventFilter.java
@@ -19,12 +19,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that the method is a filter for {@link io.kubernetes.client.informer.EventType#DELETED}
- * from watches.
+ * Indicates that the method is a filter for {@link
+ * io.kubernetes.client.informer.EventType#MODIFIED} from watches.
  *
- * <p>A delete-event filter must have the signature as {@link
- * java.util.function.BiPredicate<ApiType,ApiType>} where the 1st parameter is the old state of the
- * resource and the 2nd is the new state.
+ * <p>A update-event filter must have the signature as {@link
+ * java.util.function.BiPredicate<ApiType, Boolean>} where the 1st parameter is the
+ * "before-deletion" state of the resource and the 2nd indicates whether the cache entry for the
+ * resource is stale.
  */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
