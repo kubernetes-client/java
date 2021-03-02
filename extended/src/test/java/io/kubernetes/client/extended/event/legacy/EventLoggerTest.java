a10b0bd1be163fbab537b77381306f8708c4b9c1
yue9944882 <291271447@qq.com>
2021-03-02 15:06:04 +0800

    fixes eventlogger timestamp format

---
+++ extended/src/test/java/io/kubernetes/client/extended/event/legacy/EventLoggerTest.java
@@ -0,0 +1,32 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package io.kubernetes.client.extended.event.legacy;
+
+import static org.junit.Assert.*;
+
+import io.kubernetes.client.custom.V1Patch;
+import java.time.OffsetDateTime;
+import org.junit.Test;
+
+public class EventLoggerTest {
+
+  @Test
+  public void buildEventPatch() {
+    String expectedStr = "2021-03-02T15:02:48.179000Z";
+    OffsetDateTime expected = OffsetDateTime.parse("2021-03-02T15:02:48.179000Z");
+    V1Patch patch = EventLogger.buildEventPatch(1, "foo", expected);
+    assertEquals(
+        "{\"message\":\"foo\",\"count\":1,\"lastTimestamp\":\"" + expectedStr + "\"}",
+        patch.getValue());
+  }
+}
