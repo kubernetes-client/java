839ccf0f123d7a0c20b318f876defd16f824cc6f
yue9944882 <291271447@qq.com>
2021-10-08 15:18:59 +0800

    pin gen commit and bump 1.0.5

--- client-java-contrib/admissionreview/generate.sh
+++ client-java-contrib/admissionreview/generate.sh
@@ -5,5 +5,5 @@ docker run --rm \
   -v $LOCAL_MANIFEST_FILE:$LOCAL_MANIFEST_FILE \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $PWD:$PWD \
-  -it ghcr.io/yue9944882/crd-model-gen:v1.0.4 \
+  -it docker.pkg.github.com/kubernetes-client/java/crd-model-gen:v1.0.5 \
   sh -c "source /gen/openapi/java-crd-cmd.sh -n v1 -p io.kubernetes.client.admissionreview -l 2 -o $PWD/gen < $LOCAL_MANIFEST_FILE"
