5959a5f73985f633ea81a50841183fc4b4c5c31a
James Zheng <hippozheng@gmail.com>
2023-04-07 09:59:40 +0800

    Fix the metadata.annotations too long issue

--- client-java-contrib/generate.sh
+++ client-java-contrib/generate.sh
@@ -49,7 +49,7 @@ kind create cluster
 # install CRDs to the KinD cluster and dump the swagger spec
 for url in "${CRD_URLS[@]}"; do
   if [[ ! -z $url ]]; then
-    kubectl apply -f "$url"
+    kubectl create -f "$url"
   fi
 done
 
