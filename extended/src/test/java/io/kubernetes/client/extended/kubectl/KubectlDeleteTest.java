ba7d54c40334d84e48a05c5e8af04bb98b5978d7
Zain <zainhmoustafa@gmail.com>
2023-11-24 19:14:35 -0500

    ran spotless

--- extended/src/test/java/io/kubernetes/client/extended/kubectl/KubectlDeleteTest.java
+++ extended/src/test/java/io/kubernetes/client/extended/kubectl/KubectlDeleteTest.java
@@ -10,6 +10,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+package io.kubernetes.client.extended.kubectl;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
