cd5a0d08df73b84101fbca47f292f090df87cc2f
jan.kozlowski <jan.kozlowski@allegro.com>
2024-03-04 09:48:16 +0100

    crd-model gen v2.0.0

--- client-java-contrib/publish-gen-image.sh
+++ client-java-contrib/publish-gen-image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GEN_IMAGE_NAME=docker.pkg.github.com/kubernetes-client/java/crd-model-gen
-GEN_IMAGE_VERSION=v1.0.7
+GEN_IMAGE_VERSION=v2.0.0
 
 docker build -t ${GEN_IMAGE_NAME}:${GEN_IMAGE_VERSION} .
 docker push ${GEN_IMAGE_NAME}:${GEN_IMAGE_VERSION}
