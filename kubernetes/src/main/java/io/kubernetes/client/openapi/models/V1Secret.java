2986b2e605d3e636a1496d2ab5e526d9b232d188
Kubernetes Publisher <k8s-publishing-bot@users.noreply.github.com>
2024-04-23 13:45:18 +0000

    Applied patches under scripts/patches/*.diff
    Signed-off-by: Kubernetes Publisher <k8s-publishing-bot@users.noreply.github.com>

--- kubernetes/src/main/java/io/kubernetes/client/openapi/models/V1Secret.java
+++ kubernetes/src/main/java/io/kubernetes/client/openapi/models/V1Secret.java
@@ -20,6 +20,7 @@ import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import io.kubernetes.client.custom.MapUtils;
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import java.io.IOException;
 import java.util.HashMap;
@@ -260,7 +261,7 @@ public class V1Secret implements io.kubernetes.client.common.KubernetesObject {
     }
     V1Secret v1Secret = (V1Secret) o;
     return Objects.equals(this.apiVersion, v1Secret.apiVersion) &&
-        Objects.equals(this.data, v1Secret.data) &&
+        MapUtils.equals(this.data, v1Secret.data) &&
         Objects.equals(this.immutable, v1Secret.immutable) &&
         Objects.equals(this.kind, v1Secret.kind) &&
         Objects.equals(this.metadata, v1Secret.metadata) &&
