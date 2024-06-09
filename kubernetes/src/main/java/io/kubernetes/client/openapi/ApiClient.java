5209cf7b0d36e387c33e43bbaa5f623f69fc0180
Min Jin <minkimzz@amazon.com>
2024-06-08 19:28:47 -0700

    bump root pom to 21.0.0-SNAPSHOT
    Signed-off-by: Min Jin <minkimzz@amazon.com>

--- kubernetes/src/main/java/io/kubernetes/client/openapi/ApiClient.java
+++ kubernetes/src/main/java/io/kubernetes/client/openapi/ApiClient.java
@@ -139,7 +139,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Kubernetes Java Client/20.0.0-SNAPSHOT");
+        setUserAgent("Kubernetes Java Client/21.0.0-SNAPSHOT");
 
         authentications = new HashMap<String, Authentication>();
     }
