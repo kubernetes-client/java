07f4ed216664a966fdb19b5112eaf0fde17717e8
Ambar Mishra <mishraambar19@gmail.com>
2025-08-23 21:40:39 +0530

    Remove UserAgentTest as it's not needed

--- fluent-gen/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# TODO: Incorporate this into the pom.xml
-
-rm -r ../fluent/src/main/java/io
-mvn -Pfluent-gen clean compile
-mv target/generated-sources/annotations/io ../fluent/src/main/java/io
